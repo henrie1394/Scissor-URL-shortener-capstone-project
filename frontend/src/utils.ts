7533c7718931c47d3fff0f080a8379664b89bc82
henrie1394 <henry.etuk13@gmail.com>
2024-08-14 22:22:45 +0100

    first commit

---
+++ frontend/src/utils.ts
@@ -0,0 +1,9 @@
+// const Config = {
+// 	API_URL: "http://localhost:5000/api",
+// 	BASE_URL: "http://localhost:5000",
+// };
+const Config = {
+	API_URL: "https://arowobenedict.tech/api",
+	BASE_URL: "https://arowobenedict.tech",
+};
+export default Config;
